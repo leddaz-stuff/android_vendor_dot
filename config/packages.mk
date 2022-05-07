cd85fa547c7c5f5e04b5cd3f95045711dc5b7bc8
LeddaZ <leonardoledda@gmail.com>
2022-05-07 09:14:58 +0200

    dot: use themed icons from Lawnicons

--- config/packages.mk
+++ config/packages.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Lawnicons
+$(call inherit-product, vendor/lawnicons/product.mk)
+
 # Required packages
 PRODUCT_PACKAGES += \
     BluetoothExt \
